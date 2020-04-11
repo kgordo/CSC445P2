18444a40d16891a1f731af92a5f3d49c1cccdedc
untitled01exe <mfernan4@oswego.edu>
2020-04-10 20:48:31 -0400

    New slideing window
    WOAH

---
+++ src/NewSlideingWindowModel/PacketCatcher.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.util.Arrays;
+
+public class PacketCatcher implements Runnable {
+    DatagramSocket socket;
+
+    public PacketCatcher(DatagramSocket socket){
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        while(Shared.work){
+	    //thread stops when system stops
+            byte[] rec = new byte[2];
+            DatagramPacket recPak = new DatagramPacket(rec, rec.length);
+            try {
+                socket.receive(recPak);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            ByteBuffer buffer = ByteBuffer.wrap(recPak.getData());
+            short add = buffer.getShort();
+            if(!Shared.acks.contains(add)) {
+		//avoid dupes for any ack
+                Shared.acks.set(add, add);
+		//sets the shared int array to confirm that the thread has received an ack,
+        	//and hopefully push the left value forward
+                System.out.println("receved: " + add);
+                System.out.println(Arrays.toString(Shared.acks.toArray()) + "   " + Shared.acks.size());
+            }
+        }
+    }
+}
